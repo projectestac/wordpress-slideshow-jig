3bee79e3f7c612a92a9b6b3e8279560cf05462e7
Xavier Meler <jmeler@xtec.cat>
2015-04-30 18:32:35 +0200

    Slideshow: added proportional mode

--- views/SlideshowPluginPostType/settings.php
+++ views/SlideshowPluginPostType/settings.php
@@ -6,23 +6,19 @@
 
 		<?php if( !isset($value, $value['type'], $value['default'], $value['description']) || !is_array($value)) continue; ?>
 
-		<?php
-		//XTEC ************ ELIMINAT - Hide group information - not esencial
-		//2014.11.20 @jmeler
-		/*
-		if(!empty($value['group']) && !isset($groups[$value['group']])): $groups[$value['group']] = true; ?>
-		<tr>
-			<td colspan="3" style="border-bottom: 1px solid #e5e5e5; text-align: center;">
-				<span style="display: inline-block; position: relative; top: 14px; padding: 0 12px; background: #fff;">
-					<?php echo $value['group']; ?> <?php _e('settings', 'slideshow-jquery-image-gallery'); ?>
-				</span>
-			</td>
-		</tr>
-		<tr>
-			<td colspan="3"></td>
-		</tr>
-		<?php endif;
-		//FI ************/ ?>
+	<?php if(!empty($value['group']) && !isset($groups[$value['group']])): $groups[$value['group']] = true; ?>
+	<tr>
+		<td colspan="3" style="border-bottom: 1px solid #e5e5e5; text-align: center;">
+			<span style="display: inline-block; position: relative; top: 14px; padding: 0 12px; background: #fff;">
+				<?php echo $value['group']; ?> <?php _e('settings', 'slideshow-plugin'); ?>
+			</span>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="3"></td>
+	</tr>
+	<?php endif; ?>
+
 		<?php
 		// XTEC ************ MODIFICAT - Hide secundary options
 		// 2014.10.08 @jmeler
@@ -30,7 +26,6 @@
 				array('slideSpeed',
 					'descriptionSpeed',
 					'maxWidth',
-					'enableResponsiveness',
 					'waitUntilLoaded',
 					'showLoadingIcon',
 					'avoidFilter')))  {
